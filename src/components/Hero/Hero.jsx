33a057c03b3d043720d8b4757ea5ac9cf0f0356b
MaulanaHidayatulloh <ulyadbaim@gmail.com>
2023-11-20 18:16:15 +0700

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Maulana Hidayatulloh Mujanah</h1>
+        <p className={styles.description}>
+          I am a student at Gunadarma University, this is my portfolio using
+          React. Reach out if you'd like to learn more!
+        </p>
+        <a
+          href="mailto:maulhidayatulloh@gmail.com"
+          className={styles.contactBtn}
+        >
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/profil.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
